2ca6f978faace9b8ea0077eb5ee20d6faf2f417b
fmontalt <2dam.fmontalt@institutoserpis.org>
2017-12-11 13:31:15 +0100

    Terminado DAO pendiente Main

--- GArticulo/src/main/java/serpis/ad/ArticuloDao.java
+++ GArticulo/src/main/java/serpis/ad/ArticuloDao.java
@@ -6,17 +6,22 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class ArticuloDao {
+	
 	private static Connection connection;
 	public static void init(Connection connection) {
 		ArticuloDao.connection = connection;
 	}
+	
 	private static PreparedStatement selectIdPreparedStatement;
 	private static String selectIdSql = 
 			"select * from articulo where id=?";
 	
 	public static Articulo get(long id) throws SQLException {
-		if (selectIdPreparedStatement == null)
+		
+		if (selectIdPreparedStatement == null) {
 			selectIdPreparedStatement = connection.prepareStatement(selectIdSql);
+		}
+			
 		selectIdPreparedStatement.setObject(1, id);
 		ResultSet resultSet=selectIdPreparedStatement.executeQuery();
 		resultSet.next();
@@ -30,15 +35,54 @@ public class ArticuloDao {
 	
 	private static String insertSql = 
 			"insert into articulo (nombre, precio, categoria) values (?, ?, ?)";
+	
 		private static PreparedStatement insertPreparedStatement;
+		
 		private static void insert(Articulo articulo) throws SQLException {
-			if (insertPreparedStatement == null)
+			
+			if (insertPreparedStatement == null) {
 				insertPreparedStatement = connection.prepareStatement(insertSql);
+			}
+				
 			insertPreparedStatement.setObject(1, articulo.getNombre());
 			insertPreparedStatement.setObject(2, articulo.getPrecio());
 			insertPreparedStatement.setObject(3, articulo.getCategoria());
 			int affected=insertPreparedStatement.executeUpdate();
 			System.out.println("\n Número de líneas afectadas: "+affected+"\n");
 		}
+		
+	private static String updateSql = 
+			"update articulo set nombre = ?, precio = ?, categoria = ? where id = ?";
+	private static  PreparedStatement updatePreparedStatement;
+	
+	private static void update(Articulo articulo) throws SQLException {
+		if (updatePreparedStatement == null) {
+			updatePreparedStatement = connection.prepareStatement(updateSql);
+		}
+		updatePreparedStatement.setObject(1, articulo.getNombre());
+		updatePreparedStatement.setObject(2, articulo.getPrecio());
+		updatePreparedStatement.setObject(3, articulo.getCategoria());
+		updatePreparedStatement.setObject(4, articulo.getId());
+		
+		int affected=updatePreparedStatement.executeUpdate();
+		System.out.println("\n Número de líneas afectadas: "+affected+"\n");
+		
+	}
+	
+	private static String deleteSql = 
+			"delete from articulo where id = ?";
+	private static  PreparedStatement deletePreparedStatement;
+	private static void delete(Articulo articulo) throws SQLException {
+		if (deletePreparedStatement == null) {
+			deletePreparedStatement = connection.prepareStatement(deleteSql);
+		}
+		deletePreparedStatement.setObject(1, articulo.getId());
+		int affected=deletePreparedStatement.executeUpdate();
+		System.out.println("\n Número de líneas afectadas: "+affected+"\n");
+		
+	}
+	
+	
+	
 	
 }
--- GArticulo/src/main/java/serpis/ad/ArticuloMain.java
+++ GArticulo/src/main/java/serpis/ad/ArticuloMain.java
@@ -4,66 +4,117 @@ import java.lang.reflect.Field;
 import java.math.BigDecimal;
 import java.util.Scanner;
 
-
-
 public class ArticuloMain {
-	//Pruebas de clase	
-	public enum Option {Salir, Nuevo, Editar, Eliminar, Consultar, Listar};
+	// Pruebas de clase
+	public enum Option {
+		Salir, Nuevo, Editar, Eliminar, Consultar, Listar
+	};
+
 	private static Scanner scanner = new Scanner(System.in);
+
 	public static class Articulo {
-		
+
 		private long id;
 		private String nombre;
 		private BigDecimal precio;
 		private long categoria;
 	}
-	
-	
+
 	public static void main(String[] args) {
-		scan(Option.class);
-//		pruebaLong("introduce un numero: ");
-		
-		Runnable runnable = () -> nuevo();
-		runnable.run();
-		
+		Option opt = scan(Option.class);
+		Runnable runnable;
+		// pruebaLong("introduce un numero: ");
+
+		switch (opt) {
+		case Salir:
+			runnable = () -> salir();
+			runnable.run();
+			break;
+
+		case Nuevo:
+			runnable = () -> nuevo();
+			runnable.run();
+			break;
+
+		case Editar:
+			runnable = () -> editar();
+			runnable.run();
+			break;
+
+		case Eliminar:
+			runnable = () -> eliminar();
+			break;
+
+		case Consultar:
+			runnable = () -> consultar();
+			break;
+
+		case Listar:
+			runnable = () -> listar();
+			break;
+		}
+
 		showFields(Articulo.class);
-		
+
 	}
-//		public static long pruebaLong(String prueba) {
-//			while (true) 
-//				try {		
-//					System.out.println(prueba);
-//					String line = scanner.nextLine();
-//					return Long.parseLong(line);
-//				} catch (NumberFormatException ex) {
-//					System.out.println("Por favor, sólo números. Vuelve a introducir");
-//					
-//				}
-//		}
-		
-		public static <T extends Enum<T>> T scan(Class<T> enumType) {
-			T[] constants = enumType.getEnumConstants();
-			for(int index = 0; index < constants.length; index++)
-				System.out.printf("%s - %s\n", index, constants[index]);
-			String options = String.format("^[0-%s]$", constants.length -1);
-			while (true) {
-				System.out.println("Elige una opcion: ");
-				String line = scanner.nextLine();
-				if (line.matches(options))
-					return constants[Integer.parseInt(line)];
-				System.out.println("Opcion invalida. Vuelve a introducir.");	
-				}
+
+	private static Object listar() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private static Object consultar() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private static Object eliminar() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private static Object editar() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private static Object salir() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	// public static long pruebaLong(String prueba) {
+	// while (true)
+	// try {
+	// System.out.println(prueba);
+	// String line = scanner.nextLine();
+	// return Long.parseLong(line);
+	// } catch (NumberFormatException ex) {
+	// System.out.println("Por favor, sólo números. Vuelve a introducir");
+	//
+	// }
+	// }
+
+	public static <T extends Enum<T>> T scan(Class<T> enumType) {
+		T[] constants = enumType.getEnumConstants();
+		for (int index = 0; index < constants.length; index++)
+			System.out.printf("%s - %s\n", index, constants[index]);
+		String options = String.format("^[0-%s]$", constants.length - 1);
+		while (true) {
+			System.out.println("Elige una opcion: ");
+			String line = scanner.nextLine();
+			if (line.matches(options))
+				return constants[Integer.parseInt(line)];
+			System.out.println("Opcion invalida. Vuelve a introducir.");
 		}
-		public static void nuevo(){
-			//TODO implementar
-		}
-		
-		
-		private static void showFields(Class<?> type) {
-			for (Field field : type.getDeclaredFields() )
-				System.out.printf("%s - %s", field.getName(), field.getType().getName());
-		}
-		
+	}
+
+	public static void nuevo() {
+		// TODO implementar
+	}
+
+	private static void showFields(Class<?> type) {
+		for (Field field : type.getDeclaredFields())
+			System.out.printf("%s - %s", field.getName(), field.getType().getName());
+	}
+
 }
-
-
